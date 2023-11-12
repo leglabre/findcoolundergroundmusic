6951791f634c8084bd25164b699e6c9015537409
leglabre <44786580+leglabre@users.noreply.github.com>
2023-11-12 13:36:40 +0000

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function workingSite() {
-var band = ["https://open.spotify.com/album/7uDX4DjHqn19vrtTYCGcUM" 
-            "https://open.spotify.com/album/0J2TlWaut2IAlWdBRHLIHf"];  
+var band = new set(['https://open.spotify.com/album/7uDX4DjHqn19vrtTYCGcUM', 'https://open.spotify.com/album/0J2TlWaut2IAlWdBRHLIHf', 'https://open.spotify.com/album/1WXUFnkSXHvBSPyqeuZ7Qd', 'https://open.spotify.com/album/5AI0KCzgLqhLxjRSF3ZhJW]);  
 
 var generateBand = band[Math.floor(Math.random()*band.length)];
 window.open(generateBand);
